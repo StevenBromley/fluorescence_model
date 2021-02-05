00443e79b6df0e89b9568f29d8e9e67cd6e344cc
StevenBromley <35314969+StevenBromley@users.noreply.github.com>
2021-02-05 14:49:49 -0600

    Add files via upload

---
+++ v9 importing Ni model data and plotting versus comet.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Running Fluorescence Model for Ni I
+SJB
+01-28-2021
+This uses version 7 of the model
+"""
+from fluor_v9 import *
+"""
+fluor_v9 contains a number of dependencies and other useful packages:
+    numpy (np), matplotlib.pyplot (plt), os, math
+    Numerical integratin is handled internally using a simple rectangle code I have written. 
+"""
+model = np.genfromtxt('v9_ni0_synthetic.txt', delimiter = '\t')
+#model columns: col0 air wavelength, col1 vacuum wavelength, col2 intensity (arb. units)
+hyak0 = np.genfromtxt('offset_0_arcsec.tab',usecols = (0,1))
+#Let's try using the 345.86 line to normalize the model:
+norm_line = 345.86
+#Find index of that line in the relevant arrays:
+norm_model_idx = find_nearest_index(model[:,0],norm_line)
+norm_comet_idx = find_nearest_index(hyak0[:,0]/10,norm_line)
+#Find INTENSITY at that index in the relevant arrays:
+norm_int_model = model[norm_model_idx,2]
+norm_int_comet = hyak0[norm_comet_idx,1]
+#Scale factor from model -> comet:
+norm_factor = norm_int_comet / norm_int_model 
+
+
+#%%
+plt.clf()
+plt.plot(hyak0[:,0]/10,hyak0[:,1], color = 'green', label = 'Hyakutake 0 Offset')
+plt.stem(model[:,0], model[:,2] * norm_factor, label = 'Synthetic Ni I spectra')
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Intensity (arb. units)")
+plt.xlim(300,600)
---
+++ v9 running Ni model.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Running Fluorescence Model for Ni I
+SJB
+02-05-2021
+This uses version 7 of the model
+"""
+from fluor_v9 import *
+"""
+fluor_v9 contains a number of dependencies and other useful packages:
+    numpy (np), matplotlib.pyplot (plt), os, math
+    Numerical integratin is handled internally using a simple rectangle code I have written. 
+"""
+cwd = os.getcwd()
+raw_lines = np.genfromtxt('ni0_lines_processed.txt',delimiter='\t',dtype=str,skip_header=0)
+raw_levs = np.genfromtxt('ni0_levs_processed.txt',delimiter ='\t',skip_header=0,usecols=(0,1,2,3),dtype=str)
+ritz_col = 1 #required user input
+lower_col = 5 #required
+upper_col = 6 #required
+aval_col = 3 #required
+solar_dist = (1.02 * 1.496e+11) #AU converted to meters
+obj_vel =  -36.7 * 1e3 #Ikeya-Seki; ~154.5km/s ; Hyakutake: -36.7 * 1e3 #m/s
+m_species= 9.746267511e-26 #mass of Nickel in kg
+t_comet = 100 # Kelvin; temperature used for doppler profile.
+#Import the Kurucz spectra provided; units W / m^2 per nm:
+kurucz_flux = np.genfromtxt('kurucz_150nm-81um.txt',delimiter ='\t',dtype=float,skip_header=1) 
+#Calculate the integrated fluxes; this takes into account a doppler-broadedning line profile at temp t_comet by default
+fluxes = fluxes_with_profiles(kurucz_flux,raw_lines,raw_levs,lower_col,upper_col,aval_col,obj_vel,solar_dist,t_comet, m_species)
+model = fluorescence_spectra(fluxes,raw_lines,raw_levs,ritz_col,lower_col,upper_col,aval_col)  
+lines = model[1]
+#The lines comes in 3 columns: col0 air wavelength, col1 vacuum wavelength, col2 intensity (normalized such that max=1 unless specified otherwise)
+#%%
+#plotting the spectra:
+plt.clf()
+plt.figure(figsize=(16,8))
+plt.rcParams.update({'font.size': 22})
+plt.stem(lines[:,0], lines[:,2], label = 'Ni I Fluorescence Model')
+plt.xlabel("Wavelength (nm)")
+plt.grid()
+plt.ylabel("Intensity (arb. units)")
+plt.xlim(200,600)
+
+np.savetxt('v9_ni0_synthetic.txt',lines,delimiter ='\t')
